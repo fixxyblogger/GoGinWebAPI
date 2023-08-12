468b76601b4a04ade7d5ddc72aa3ce7b7d17aded
chanchaijeimvijack <kewell5@live.com>
2023-08-12 20:08:32 +0700

    initial first example

---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Book struct {
+	ID     string `json:"id"`
+	Title  string `json:"title"`
+	Author string `json:"author"`
+}
+
+var books = []Book{
+	{ID: "1", Title: "Harry Potter", Author: "J. K. Rowling"},
+	{ID: "2", Title: "The Lord of the Rings", Author: "J. R. R. Tolkien"},
+	{ID: "3", Title: "The Wizard of Oz", Author: "L. Frank Baum"},
+}
+
+// main is the entry point of the program.
+//
+// It initializes a new Gin router and defines a GET route for "/books".
+// This route returns a JSON response with the HTTP status code 200 and the
+// list of books.
+//
+// No parameters are required.
+// No return values.
+func main() {
+	r := gin.New()
+
+	r.Use(gin.Logger())
+
+	r.GET("/books", func(c *gin.Context) {
+		log.Println("Handling GET /books request")
+		c.JSON(http.StatusOK, books)
+	})
+
+	log.Println("Starting server on port 8080")
+	r.Run(":8080")
+}
